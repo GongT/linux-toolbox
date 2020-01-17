e62acdd1242e5cb204f76285c766bf954eec1ff2
GongT <admin@gongt.me>
2020-01-18 03:18:47 +0800

    ls +A

--- quick-alias/utils.sh
+++ quick-alias/utils.sh
@@ -3,10 +3,9 @@
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-alias ll='ls -l'
-alias la='ls -a'
+alias ll='LC_ALL=zh_CN.UTF-8 ls -hAl'
+alias la='LC_ALL=zh_CN.UTF-8 ls -hA'
 alias vi='vim'
-
 
 export SSH_CLIENT_IP=$(echo "${SSH_CLIENT}" | awk '{print $1}')
 
