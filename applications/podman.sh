2fe3472a9bdf6a6dc16d009888ecab8830809b7e
GongT <admin@gongt.me>
2020-01-20 14:33:51 +0800

    wrap docker with podman

--- applications/podman.sh
+++ applications/podman.sh
@@ -9,3 +9,9 @@ emit "alias podman=\"${VAR_HERE}/bin/podman_wrap '${PODMAN}'\""
 emit "alias pps=\"${VAR_HERE}/bin/podman_wrap '${PODMAN}' pps\""
 emit "alias pmg=\"${VAR_HERE}/bin/podman_wrap '${PODMAN}' pmg\""
 
+if ! command_exists docker ; then
+	echo '#!/bin/sh
+exec podman "$@"
+' > "${GEN_BIN_PATH}/docker"
+	chmod a+rx "${GEN_BIN_PATH}/docker"
+fi
