92a31985508d93144666ecfba7a7796830c7e56d
GongT <admin@gongt.me>
2020-09-28 03:44:39 +0800

    update

--- applications/git.sh
+++ applications/git.sh
@@ -3,11 +3,12 @@ if ! command_exists git ; then
 fi
 
 emit_file alias/git.sh
+copy_bin bin/git-find-large
 
 git config --system core.autocrlf input
 git config --system core.eol lf
 # git config --system 'url "file://".insteadOf' git+file://
 git config --system push.default upstream
-git config --system submodule.recurse true
+# git config --system submodule.recurse true
 git config --system fetch.recurseSubmodules on-demand
 git config --system pull.rebase true
