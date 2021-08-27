88eb73d081d5b6e0f610525a3fc0653b9475d4f0
GongT <admin@gongt.me>
2021-08-27 17:16:17 +0800

    fix vscode

--- common/special/vscode-server.sh
+++ common/special/vscode-server.sh
@@ -1,85 +1,90 @@
-if [[ "$USERNAME" ]] && [[ "$(id -u)" -eq 0 ]] && ! [[ "${VSCODE_IPC_HOOK_CLI:-}" ]]; then
-	for i in $(seq 0 8); do
-		echo "|" >&2
-	done
-	export USERNAME=''
-	unset USERNAME
-	echo "Detected VSCode session; USERNAME=$USERNAME; SSH process PID is $$" >&2
-	echo "Bash Options: $- ; Arguments ($#): $*" >&2
+while true; do
+	if [[ "$USERNAME" ]] && [[ "$(id -u)" -eq 0 ]] && ! [[ "${VSCODE_IPC_HOOK_CLI:-}" ]]; then
+		for i in $(seq 0 8); do
+			echo "|" >&2
+		done
+		export USERNAME=''
+		unset USERNAME
+		echo "Detected VSCode session; USERNAME=$USERNAME; SSH process PID is $$" >&2
+		echo "Bash Options: $- ; Arguments ($#): $*" >&2
 
-	declare -rx LCODE_LIBEXEC="/usr/local/libexec/linux-toolbox/vscode-wrap"
-	if [[ ${VSCODE_SERVER_HACK_ROOT+found} != found ]]; then
-		export VSCODE_SERVER_HACK_ROOT=/data/AppData/VSCodeRemote
-	fi
-	if [[ ! $VSCODE_SERVER_HACK_ROOT ]]; then
-		echo "Remount vscode server has disabled" >&2
-	fi
-	echo "VSCode Server files save to: $VSCODE_SERVER_HACK_ROOT" >&2
+		declare -rx LCODE_LIBEXEC="/usr/local/libexec/linux-toolbox/vscode-wrap"
+		if [[ ${VSCODE_SERVER_HACK_ROOT+found} != found ]]; then
+			export VSCODE_SERVER_HACK_ROOT=/data/AppData/VSCodeRemote
+		fi
+		if [[ ! $VSCODE_SERVER_HACK_ROOT ]]; then
+			echo "Remount vscode server has disabled" >&2
+			break
+		fi
+		echo "VSCode Server files save to: $VSCODE_SERVER_HACK_ROOT" >&2
 
-	declare -x TMPDIR="/tmp/vscode-server"
-	mkdir -p "$TMPDIR"
+		declare -x TMPDIR="/tmp/vscode-server"
+		mkdir -p "$TMPDIR"
 
-	if [[ "$PROXY" ]]; then
-		export http_proxy="$PROXY" https_proxy="$PROXY"
-		echo "Using proxy: $PROXY" >&2
-	fi
+		if [[ "$PROXY" ]]; then
+			export http_proxy="$PROXY" https_proxy="$PROXY"
+			echo "Using proxy: $PROXY" >&2
+		fi
 
-	if mountpoint -q "$HOME/.vscode-server"; then
-		echo "Already in namespace: $HOME/.vscode-server is mountpoint" >&2
+		if mountpoint -q "$HOME/.vscode-server"; then
+			echo "Already in namespace: $HOME/.vscode-server is mountpoint" >&2
+			break
+		fi
+
+		mapfile -t PORTS < <(
+			find "$VSCODE_SERVER_HACK_ROOT" -maxdepth 1 -name ".*.log" \
+				| xargs cat | grep -oE 'listening on [[:digit:]]+' \
+				| grep -oE '[[:digit:]]+'
+		)
+
+		replace_bash() {
+			export TARGET_PID="$1"
+			export PATH="$LCODE_LIBEXEC:$PATH"
+			bash() {
+				echo "[CALL] bash $*" >&2
+				set -x
+				tee "$TMPDIR/download-install-script.sh" \
+					| nsenter --target "$TARGET_PID" --mount /usr/bin/bash "$@"
+			}
+			echo "BASH replaced. [target pid $TARGET_PID]" >&2
+		}
+
+		for P in "${PORTS[@]}"; do
+			PID=$(lsof -n -i ":$P" | grep LISTEN | awk '{print $2}')
+			if [[ "$PID" ]]; then
+				echo "Found vscode remote server process: $PID" >&2
+				replace_bash "$PID"
+				return
+			fi
+		done
+
+		echo "Did not found any running server..." >&2
+		declare -r PIDFILE="/run/vscode-server-prepare-result.pid"
+
+		if [[ -e $PIDFILE ]]; then
+			echo "Pid file $PIDFILE exists." >&2
+			if nsenter --target "$(<"$PIDFILE")" --all mountpoint "$HOME/.vscode-server" &>/dev/null; then
+				echo "    And valid" >&2
+				replace_bash "$(<"$PIDFILE")"
+				return
+			else
+				echo "    But invalid" >&2
+			fi
+		fi
+
+		{
+			echo "ENV PATH:"
+			echo -n '  | '
+			echo "$PATH" | sed 's/:/\n  | /g'
+		} >&2
+
+		rm -f "$PIDFILE"
+		unshare --mount --propagation slave bash "$LCODE_LIBEXEC/_vscode-server-prepare.sh" "$PIDFILE" &
+		sleep 5
+
+		replace_bash "$(<"$PIDFILE")"
 		return
 	fi
 
-	mapfile -t PORTS < <(
-		find "$VSCODE_SERVER_HACK_ROOT" -maxdepth 1 -name ".*.log" \
-			| xargs cat | grep -oE 'listening on [[:digit:]]+' \
-			| grep -oE '[[:digit:]]+'
-	)
-
-	replace_bash() {
-		export TARGET_PID="$1"
-		export PATH="$LCODE_LIBEXEC:$PATH"
-		bash() {
-			echo "[CALL] bash $*" >&2
-			set -x
-			tee "$TMPDIR/download-install-script.sh" \
-				| nsenter --target "$TARGET_PID" --mount /usr/bin/bash "$@"
-		}
-		echo "BASH replaced. [target pid $TARGET_PID]" >&2
-	}
-
-	for P in "${PORTS[@]}"; do
-		PID=$(lsof -n -i ":$P" | grep LISTEN | awk '{print $2}')
-		if [[ "$PID" ]]; then
-			echo "Found vscode remote server process: $PID" >&2
-			replace_bash "$PID"
-			return
-		fi
-	done
-
-	echo "Did not found any running server..." >&2
-	declare -r PIDFILE="/run/vscode-server-prepare-result.pid"
-
-	if [[ -e $PIDFILE ]]; then
-		echo "Pid file $PIDFILE exists." >&2
-		if nsenter --target "$(<"$PIDFILE")" --all mountpoint "$HOME/.vscode-server" &>/dev/null; then
-			echo "    And valid" >&2
-			replace_bash "$(<"$PIDFILE")"
-			return
-		else
-			echo "    But invalid" >&2
-		fi
-	fi
-
-	{
-		echo "ENV PATH:"
-		echo -n '  | '
-		echo "$PATH" | sed 's/:/\n  | /g'
-	} >&2
-
-	rm -f "$PIDFILE"
-	unshare --mount --propagation slave bash "$LCODE_LIBEXEC/_vscode-server-prepare.sh" "$PIDFILE" &
-	sleep 5
-
-	replace_bash "$(<"$PIDFILE")"
-	return
-fi
+	break
+done
