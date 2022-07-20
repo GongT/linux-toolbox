ee5a6af73be096212fa7f89ac8b38d95d1e24129
GongT <admin@gongt.me>
2022-07-20 13:27:36 +0800

    add sudo to dnf

--- system/package-manager/dnf.sh
+++ system/package-manager/dnf.sh
@@ -1,19 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 emit "export SYSTEM_PACKAGE_MANAGER=dnf"
 emit "function package-manager-make-cache {
-	dnf makecache
+	$SUDO dnf makecache
 }"
 
-if ! grep -q "keepcache" /etc/dnf/dnf.conf; then
-	echo "
-clean_requirements_on_remove=True
-keepcache=True
-exit_on_lock=True
-# ip_resolve=4
-# proxy=
-max_parallel_downloads=10
-install_weak_deps=False
-cachedir=/var/cache/dnf
-" >> /etc/dnf/dnf.conf
-fi
+dnf_config() {
+	local K="$1" V="$2"
+	if grep --quiet --fixed-strings --ignore-case "$1=" /etc/dnf/dnf.conf; then
+		return
+	fi
+
+	echo "$K=$V" >>/etc/dnf/dnf.conf
+}
+
+dnf_config clean_requirements_on_remove True
+dnf_config best False
+dnf_config keepcache True
+dnf_config exit_on_lock True
+dnf_config ip_resolve 4
+dnf_config proxy ''
+dnf_config max_parallel_downloads 10
+dnf_config install_weak_deps False
+dnf_config cachedir /var/cache/dnf
+dnf_config gpgcheck 1
+dnf_config installonly_limit 3
+dnf_config skip_if_unavailable True
+dnf_config fastestmirror True
