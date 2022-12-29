e32562faab6aa0a8fa1cd11d72faafce1d8b57b9
GongT <admin@gongt.me>
2022-12-29 08:00:52 +0800

    update podman pull

--- applications/bin/podman_wrap.sh
+++ applications/bin/podman_wrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PODMAN='/usr/bin/podman'
+
 set -Eeuo pipefail
 
 if ! [ -t 1 ] || ! [ -t 0 ]; then
@@ -10,11 +12,11 @@ BACKUP_PATH="${SYSTEM_COMMON_BACKUP:-/data/Backup}/containers"
 
 function clean_images() {
 	echo -e "\e[38;5;5mremoving images:\e[0m"
-	"${PODMAN}" images | (grep -E '<none>' || [[ $? == 1 ]]) | awk '{print $3}' | xargs --no-run-if-empty --verbose --no-run-if-empty "${PODMAN}" rmi
+	"$PODMAN" images | (grep -E '<none>' || [[ $? == 1 ]]) | awk '{print $3}' | xargs --no-run-if-empty --verbose --no-run-if-empty "$PODMAN" rmi
 }
 function clear_stopped_container() {
 	echo -e "\e[38;5;5mremoving containers:\e[0m"
-	"${PODMAN}" ps -a | tail -n +2 | (grep -v Up || [[ $? == 1 ]]) | awk '{print $1}' | xargs --no-run-if-empty --verbose --no-run-if-empty "${PODMAN}" rm
+	"$PODMAN" ps -a | tail -n +2 | (grep -v Up || [[ $? == 1 ]]) | awk '{print $1}' | xargs --no-run-if-empty --verbose --no-run-if-empty "$PODMAN" rm
 }
 
 function get_backup_label() {
@@ -36,22 +38,22 @@ function get_label_tag() {
 function do_backup() {
 	local ACTION=${1:-}
 
-	if [[ "$ACTION" == 'create' ]]; then
+	if [[ $ACTION == 'create' ]]; then
 		local IMGS=() I OUT
 
-		mapfile -t IMGS < <("${PODMAN}" images | tail -n +2 | grep -v '<none>' | awk '{print $1":"$2}')
+		mapfile -t IMGS < <("$PODMAN" images | tail -n +2 | grep -v '<none>' | awk '{print $1":"$2}')
 
 		echo "backup directory: $BACKUP_PATH"
 		for I in "${IMGS[@]}"; do
 			echo "Backup image: $I"
 			OUT="$BACKUP_PATH/${I}.tar.gz"
 			mkdir -p "$(dirname "$OUT")"
-			podman save "$I" | pv > "$OUT" || {
+			podman save "$I" | pv >"$OUT" || {
 				echo "failed."
 				return 1
 			}
 		done
-	elif [[ "$ACTION" == 'restore' ]]; then
+	elif [[ $ACTION == 'restore' ]]; then
 		mkdir -p "$BACKUP_PATH"
 
 		local FILES I TMPDIR="$TMPDIR/podman" LBL
@@ -74,22 +76,21 @@ function do_backup() {
 function pps() {
 	local FMT="$1"
 	shift
-	local OUT=$("${PODMAN}" ps --format "$FMT" -a "$@")
-	local HEAD=$(echo "${OUT}" | head -1)
-	if [[ -z "$HEAD" ]]; then
+	local OUT=$("$PODMAN" ps --format "$FMT" --all --noheading "$@")
+	if [[ ! $OUT ]]; then
 		echo "no container exists."
 		return
 	fi
-	local RUNNING=$(echo "${OUT}" | grep --color=no $'[[:space:]]Up [0-9LA]')
-	local EXITED=$(echo "${OUT}" | grep --color=no -v $'[[:space:]]Up [0-9LA]')
-	if [[ -n "$RUNNING" ]]; then
+	local RUNNING=$(echo "${OUT}" | grep --color=no '\bUp [0-9LA]')
+	local EXITED=$(echo "${OUT}" | grep --color=no -v '\bUp [0-9LA]')
+	if [[ -n $RUNNING ]]; then
 		echo -e "\e[38;5;10mrunning pods:\e[0m"
 		echo "$RUNNING"
 	fi
-	if [[ -n "$RUNNING" ]] && [[ -n "$EXITED" ]]; then
+	if [[ -n $RUNNING ]] && [[ -n $EXITED ]]; then
 		echo '------'
 	fi
-	if [[ -n "$EXITED" ]]; then
+	if [[ -n $EXITED ]]; then
 		echo -e "\e[38;5;9mstopped pods:\e[0m"
 		echo "$EXITED"
 	fi
@@ -102,16 +103,16 @@ ps)
 	;;
 pss)
 	shift
-	pps "table {{.Names}} {{.ID}} {{.Image}} {{.Status}}<nltab>Ports:{{.Ports}}<nltab>Mounts:{{.Mounts }}" "$@" \
-		| sed -E 's/<nltab>/\n\t/g; s/^\s*(Ports|Mounts):\s*\n//mg; s/(Ports|Mounts):.+/\x1B[2m\0\x1B[0m/mg; s/^\S+/\x1B[38;5;14m\0\x1B[0m/mg'
+	pps "table {{.Names}} {{.ID}} {{.Image}} {{.Status}}<nltab>Ports:{{.Ports}}<nltab>Mounts:{{.Mounts }}<nl>" "$@" \
+		| sed -E 's/<nltab>/\n\t/g; s/<nl>/\n/g; s/^\s*(Ports|Mounts):\s*\n//mg; s/(Ports|Mounts):.+/\x1B[2m\0\x1B[0m/mg; s/^\S+/\x1B[38;5;14m\0\x1B[0m/mg'
 	;;
 psss)
 	shift
-	"${PODMAN}" ps "$@"
+	"$PODMAN" ps "$@"
 	;;
 img)
 	shift
-	"${PODMAN}" images --format "table {{.ID}}\t{{.Repository}}:{{.Tag}}\t{{.Size}}" "$@"
+	"$PODMAN" images --format "table {{.ID}}\t{{.Repository}}:{{.Tag}}\t{{.Size}}" "$@"
 	;;
 clean)
 	shift
@@ -120,27 +121,35 @@ clean)
 	;;
 xrmi)
 	shift
-	WHAT=$1
-	if [[ "$WHAT" ]]; then
-		"${PODMAN}" images "$@" --noheading | awk '{print $3}' | xargs -t --no-run-if-empty "${PODMAN}" rmi
+	if [[ $# -gt 0 ]]; then
+		LIST=$("$PODMAN" images --format "{{.ID}}\t{{.Repository}}:{{.Tag}}" --noheading)
+		for I in "$@"; do
+			LIST=$(echo "$LIST" | grep "$I" || true)
+		done
+		if [[ ! $LIST ]]; then
+			echo "no image match: $*"
+			exit 0
+		fi
+
+		echo "$LIST" | awk '{print $1}' | xargs --no-run-if-empty "$PODMAN" rmi
 	else
 		echo "Usage: podman xrmi <part of image name>"
 	fi
 	;;
 --help)
 	if [[ $# -ne 1 ]]; then
-		"${PODMAN}" "$@"
+		"$PODMAN" "$@"
 	else
 		echo "This is a wrapper for 'podman'" >&2
-		"${PODMAN}" --help
+		"$PODMAN" --help
 	fi
 	;;
 pull)
 	shift
-	if [[ $# -eq 1 ]] && [[ "$1" = "all" ]]; then
-		"${PODMAN}" images | awk '{print $1":"$2}' | grep -- docker.io | xargs --no-run-if-empty -n1 -t "${PODMAN}" pull
+	if [[ $# -eq 0 ]]; then
+		"$PODMAN" images --format "{{.Repository}}:{{.Tag}}" --noheading | grep -v -E '^localhost/|example\.com/' | xargs --no-run-if-empty -n1 -t "$PODMAN" pull
 	else
-		"${PODMAN}" pull "$@"
+		"$PODMAN" pull "$@"
 	fi
 	;;
 backup)
@@ -148,6 +157,6 @@ backup)
 	do_backup "$@"
 	;;
 *)
-	"${PODMAN}" "$@"
+	"$PODMAN" "$@"
 	;;
 esac
