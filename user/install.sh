8a568120f87ae7564460fb696daf53efe798b648
GongT <admin@gongt.me>
2023-06-02 15:31:59 +0800

    fix

--- user/install.sh
+++ user/install.sh
@@ -9,22 +9,10 @@ write_sshd_config() {
 	fi
 }
 
-sshd-allow-environment() {
-	local title=$1
-	shift
-	local VALUES="$*"
-	if [[ ! $VALUES ]]; then
-		echo "Empty input!" >&2
-		return 1
-	fi
-
-	write_sshd_config "$title" "AcceptEnv $VALUES"
-}
-
 emit_file vscode.sh
 emit_file sshd.sh
 
 source sshd.sh
 VALUES="DISPLAY SSH_CLIENT_IP REMOTE_PATH SSH_ORIGIN_MACHINE VSCODE_CWD USERNAME"
-sshd-allow-environment allow-input "$VALUES"
-write_sshd_config allow-output "SendEnv $VALUES"
+write_sshd_config allow-input "AcceptEnv $VALUES"
+file-section "$HOME/.ssh/ssh_config" linux-toolbox-allow-send "SendEnv $VALUES"
