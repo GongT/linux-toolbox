6f6357af1c35bcd717e160ee7152419eba2ea5bc
GongT <admin@gongt.me>
2025-03-18 10:32:02 +0800

    try

--- applications/podman.sh
+++ applications/podman.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PODMAN=$(find_command podman)
-if [[ -n "${PODMAN}" ]]; then
+if command_exists podman; then
+	echo "podman exists"
+	PODMAN=$(find_command podman)
 	copy_bin_with_env \
 		"PODMAN='$PODMAN'" \
 		"bin/podman_wrap.sh" \
@@ -11,15 +12,19 @@ if [[ -n "${PODMAN}" ]]; then
 		cru a "podman-auto-pull" "0 */8 * * * /usr/bin/env bash '$HERE/staff/podman-pull-all.sh'" "每8小时从dockerhub更新镜像"
 	fi
 else
+	echo "podman not found"
 	if command_exists crontab; then
 		cru d "podman-cleanup" "podman-auto-pull"
 	fi
 fi
 
 if command_exists buildah; then
+	echo "podman buildah"
 	BUILDAH=$(find_command buildah)
 	copy_bin_with_env \
 		"BUILDAH='$BUILDAH'" \
 		"bin/buildah_wrap.sh" \
 		"buildah"
+else
+	echo "podman not found"
 fi
