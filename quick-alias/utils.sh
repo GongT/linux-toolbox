675797b519c4ae49c528d4aba3e9433e75358d6f
GongT <admin@gongt.me>
2025-04-05 04:12:49 +0800

    fix ls chinese files

--- quick-alias/utils.sh
+++ quick-alias/utils.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-alias ls='ls --color=auto'
-alias l.='LC_ALL=zh_CN.UTF-8 ls -d .* --color=auto'
-alias ll='LC_ALL=zh_CN.UTF-8 ls -lhA --color=auto'
-alias la='LC_ALL=zh_CN.UTF-8 ls -hA --color=auto'
+if [[ -t 1 ]]; then
+	function ls() {
+		LC_ALL=zh_CN.UTF-8 command ls --color=auto "$@"
+	}
+else
+	function ls() {
+		LC_ALL=zh_CN.UTF-8 command ls "$@"
+	}
+fi
+alias l.='ls -d .*'
+alias ll='ls -lhA'
+alias la='ls -hA'
 alias vi='vim'
 
 # Interactive operation...
@@ -16,8 +24,8 @@ alias df='df -h'
 alias du='du -h'
 
 # Misc :)
-alias less='less -r'                          # raw control characters
-alias whence='type -a'                        # where, of a sort
-alias grep='grep --color'                     # show differences in colour
-alias egrep='egrep --color=auto'              # show differences in colour
-alias fgrep='fgrep --color=auto'              # show differences in colour
+alias less='less -r'             # raw control characters
+alias whence='type -a'           # where, of a sort
+alias grep='grep --color'        # show differences in colour
+alias egrep='egrep --color=auto' # show differences in colour
+alias fgrep='fgrep --color=auto' # show differences in colour
