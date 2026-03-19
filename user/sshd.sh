147db4f669c83c5c37fe1dc0848f4f182878b1f0
���� <admin@gongt.me>
2026-03-20 01:23:29 +0800

    auto convert gbk

--- user/sshd.sh
+++ user/sshd.sh
@@ -12,3 +12,15 @@ if [[ ! ${DISPLAY:-} ]]; then
 		export DISPLAY=":0"
 	fi
 fi
+
+if [[ ${USER_DISPLAYNAME-} ]]; then
+	if ! echo "$USER_DISPLAYNAME" | iconv -f UTF-8 &>/dev/null ; then
+		USER_DISPLAYNAME=$(echo "$USER_DISPLAYNAME" | iconv -f GB18030 -t UTF-8)
+
+		if [[ -t 2 ]]; then
+			printf "\e[38;5;11m%s\e[0m\n" "warning: USER_DISPLAYNAME is not valid UTF-8, assuming it uses GB18030: '$USER_DISPLAYNAME'" >&2
+		fi
+	fi
+
+	export USER_DISPLAYNAME
+fi
